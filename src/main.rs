2633cf0c9186940fbb33719dac6860288c317940
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-11 14:04:00 -0700

    Get config file reading working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use serde::{Deserialize, Serialize};
 use std::env;
-use toml::{de::Error, Value};
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use toml::{de, Value};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct File {
+struct Dot {
+    name: String,
     origin: String,
     deployed: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
-    files: Vec<File>,
+    files: Vec<Dot>,
 }
 
 enum Verbs {
@@ -27,15 +31,26 @@ fn get_verb(verb: &str) -> Verbs {
     }
 }
 
-fn main() -> Result<(), Error> {
+fn open_config() -> Result<(), io::Error> {
+    let mut config_file = File::open("/home/jake/.config/dotsin/dotsin.toml")?;
+    let mut config = String::new();
+    config_file.read_to_string(&mut config)?;
+    let config_toml: Config = toml::from_str(config.as_str()).unwrap();
+    println!("{:#?}", config_toml);
+    Ok(())
+}
+
+fn main() -> Result<(), de::Error> {
     let mut argv = env::args();
     let argc = argv.len();
 
     if argc == 1 {
         println!("One arg");
     } else if argc >= 2 {
-        let verb = get_verb(argv.nth(1).unwrap().as_str());
+        let verb: Verbs = get_verb(argv.nth(1).unwrap().as_str());
     }
+
+    open_config();
 
     Ok(())
 }
