61b93a30a507ba3bd56b73d6c5f5c0427fe1f941
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-11 14:07:17 -0700

    Start config logic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::fs::File;
 use std::io;
 use std::io::Read;
-use toml::{de, Value};
+use toml::de;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Dot {
@@ -31,13 +31,12 @@ fn get_verb(verb: &str) -> Verbs {
     }
 }
 
-fn open_config() -> Result<(), io::Error> {
+fn open_config() -> Result<Config, io::Error> {
     let mut config_file = File::open("/home/jake/.config/dotsin/dotsin.toml")?;
     let mut config = String::new();
     config_file.read_to_string(&mut config)?;
     let config_toml: Config = toml::from_str(config.as_str()).unwrap();
-    println!("{:#?}", config_toml);
-    Ok(())
+    Ok(config_toml)
 }
 
 fn main() -> Result<(), de::Error> {
@@ -47,10 +46,9 @@ fn main() -> Result<(), de::Error> {
     if argc == 1 {
         println!("One arg");
     } else if argc >= 2 {
-        let verb: Verbs = get_verb(argv.nth(1).unwrap().as_str());
+        let _verb: Verbs = get_verb(argv.nth(1).unwrap().as_str());
     }
 
-    open_config();
-
+    let _config = open_config().unwrap();
     Ok(())
 }
