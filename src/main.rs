5c0f9d4bf0a300f172aeea2a1d2e1d19e0cd6bf1
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-14 01:17:56 -0700

    Add tests instead of fixing moving full dirs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::cmp::PartialEq;
 use std::env;
-use std::fs::{copy, File};
+use std::ffi::OsStr;
+use std::fs::{copy, create_dir_all, read_dir, File};
 use std::io;
 use std::io::{stdin, Read};
+use std::path::Path;
 use toml::de;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -18,7 +20,7 @@ struct Config {
     files: Vec<Dot>,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Verbs {
     Deploy,
     Save,
@@ -51,8 +53,8 @@ fn get_message_from_dot(verb: &Verbs, dot: &Dot) -> String {
 /// Ask a message for a dot according to the verb and get a yes or no response
 /// Return a bool depending on the yes or no
 fn ask(verb: &Verbs, dot: &Dot) -> Result<bool, io::Error> {
-    let mut line = String::new();
-    let message = get_message_from_dot(&verb, &dot);
+    let mut line: String = String::new();
+    let message: String = get_message_from_dot(&verb, &dot);
 
     println!("{}", message);
     stdin().read_line(&mut line)?;
@@ -78,11 +80,33 @@ fn action_for_dot(
     Ok(())
 }
 
+/// Move a file or directory
+fn safely_copy(
+    from: impl AsRef<Path>,
+    to: impl AsRef<Path> + AsRef<OsStr>,
+) -> Result<(), io::Error> {
+    if !Path::new(&to).exists() {
+        create_dir_all(&to)?;
+    }
+
+    // for entry in read_dir(from)? {
+    //     let entry = entry?;
+    //     if entry.file_type()?.is_dir() {
+    //         let next_to: impl AsRef<Path> + AsRef<OsStr> = to.as_ref().join(entry.file_name());
+    //         safely_copy(entry.path(), next_to)?;
+    //     } else {
+    //         copy(entry.path(), to.as_ref().join(entry.file_name()))?;
+    //     }
+    // }
+
+    Ok(())
+}
+
 /// Ask for each dot file to run save_inner on it
 fn save(config: &Config, verb: &Verbs) -> Result<(), io::Error> {
     /// Copy the deployed file to the origin location
     fn save_inner(dot: &Dot) -> Result<(), io::Error> {
-        copy(&dot.deployed, &dot.origin)?;
+        safely_copy(&dot.deployed, &dot.origin)?;
         Ok(())
     }
     action_for_dot(&config, &save_inner, &verb)?;
@@ -94,6 +118,7 @@ fn deploy(config: &Config, verb: &Verbs) -> Result<(), io::Error> {
     /// Copy the origin file to the deployed location
     fn deploy_inner(dot: &Dot) -> Result<(), io::Error> {
         copy(&dot.origin, &dot.deployed)?;
+        println!("Successfully deployed {}!", dot.name);
         Ok(())
     }
     action_for_dot(&config, &deploy_inner, &verb)?;
@@ -110,7 +135,7 @@ fn diff(config: &Config, verb: &Verbs) {
 /// Open the configuration file as a toml struct
 fn open_config() -> Result<Config, io::Error> {
     let mut config_file = File::open("/home/jake/.config/stow-squid/stow-squid.toml")?;
-    let mut config = String::new();
+    let mut config: String = String::new();
     config_file.read_to_string(&mut config)?;
     let config_toml: Config = toml::from_str(config.as_str()).unwrap();
     Ok(config_toml)
@@ -118,12 +143,12 @@ fn open_config() -> Result<Config, io::Error> {
 
 fn main() -> Result<(), de::Error> {
     let mut argv = env::args();
-    let argc = argv.len();
+    let argc: usize = argv.len();
 
     if argc == 1 {
         println!("One arg");
     } else if argc >= 2 {
-        let config = open_config().unwrap();
+        let config: Config = open_config().unwrap();
 
         let verb: Verbs = get_verb(argv.nth(1).unwrap().as_str());
         if verb == Verbs::Save {
@@ -136,4 +161,38 @@ fn main() -> Result<(), de::Error> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_verb_test() {
+        assert_eq!(get_verb("save"), Verbs::Save);
+        assert_eq!(get_verb("deploy"), Verbs::Deploy);
+        assert_eq!(get_verb("diff"), Verbs::Diff);
+        assert_eq!(get_verb("something"), Verbs::None);
+        assert_eq!(get_verb("something else"), Verbs::None);
+        assert_eq!(get_verb("anything"), Verbs::None);
+    }
+
+    #[test]
+    fn get_message_from_dot_test() {
+        let dot: Dot = Dot {
+            name: "bspwm".to_string(),
+            origin: "this".to_string(),
+            deployed: "that".to_string(),
+        };
+        assert_eq!(
+            get_message_from_dot(&Verbs::Save, &dot),
+            "Would you like to save that? "
+        );
+        assert_eq!(
+            get_message_from_dot(&Verbs::Deploy, &dot),
+            "Would you like to deploy this -> that? "
+        );
+        assert_eq!(get_message_from_dot(&Verbs::None, &dot), "");
+        assert_eq!(get_message_from_dot(&Verbs::Diff, &dot), "");
+    }
 }
