7c5d32ea6e71938f804714fa8cefe3f1fc9d5c8b
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-14 22:06:26 -0700

    Add tests to verb

--- src/verb.rs
+++ src/verb.rs
@@ -30,3 +30,37 @@ pub fn get_message_from_dot(verb: &Verbs, dot: &Dot) -> String {
         _ => "".to_string(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_verb_test() {
+        assert_eq!(get_verb("save"), Verbs::Save);
+        assert_eq!(get_verb("deploy"), Verbs::Deploy);
+        assert_eq!(get_verb("diff"), Verbs::Diff);
+        assert_eq!(get_verb("something"), Verbs::None);
+        assert_eq!(get_verb("something else"), Verbs::None);
+        assert_eq!(get_verb("anything"), Verbs::None);
+    }
+
+    #[test]
+    fn get_message_from_dot_test() {
+        let dot: Dot = Dot {
+            name: "bspwm".to_string(),
+            origin: "this".to_string(),
+            deployed: "that".to_string(),
+        };
+        assert_eq!(
+            get_message_from_dot(&Verbs::Save, &dot),
+            "Would you like to save that? "
+        );
+        assert_eq!(
+            get_message_from_dot(&Verbs::Deploy, &dot),
+            "Would you like to deploy this -> that? "
+        );
+        assert_eq!(get_message_from_dot(&Verbs::None, &dot), "");
+        assert_eq!(get_message_from_dot(&Verbs::Diff, &dot), "");
+    }
+}
